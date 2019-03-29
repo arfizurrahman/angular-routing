d9ad8dfbfa60e1e11e75acbd4c9fd799ab315c42
Arfizur Rahman <arfizrahman0@gmail.com>
2019-03-30 00:53:32 +0600

    Add existing files

---
+++ src/app/auth.service.ts
@@ -0,0 +1,23 @@
+
+
+export class AuthService {
+    loggedIn = false;
+
+    iaAuthenticated() {
+        const promise = new Promise(
+            (resolve, reject) => {
+                setTimeout(() => {
+                    resolve(this.loggedIn);
+                }, 800);
+            }
+        );
+        return promise;
+    }
+    loggin() {
+        this.loggedIn = true;
+    }
+
+    loggout() {
+        this.loggedIn = false;
+    }
+}
